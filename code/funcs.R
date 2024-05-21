4e9ded69108d5eded2135a0a5db36dd761b5bdb5
Your Name <jeffrey.sj.tang@gmail.com>
2024-05-21 11:38:19 -0700

    add extract db function in funcs.R

--- code/funcs.R
+++ code/funcs.R
@@ -1,3 +1,19 @@
+# Helper fun to filter db and extract ligands-receptors pairs match
+extract_db <- function(cytokine, eset, dbs) {
+  # For all databases, search for ligand that matched cytokine and eset
+  genes <- rownames(eset)
+  receptors_interest <- c(cytokine, genes)
+  # Now we could databases based on the rownames of eset and cytokine
+  output_db <- lapply(dbs, function(db) {
+    # Then filter on db as well
+    db <- db[names(db) %in% receptors_interest] |> 
+      lapply(function(i) { intersect(i, genes) })
+    filter_db <- db[sapply(db, length) > 0]
+    return(filter_db)
+  })
+  return(output_db)
+}
+
 ## enrichment functions
 
 create_design <- function(y, obs_id){
