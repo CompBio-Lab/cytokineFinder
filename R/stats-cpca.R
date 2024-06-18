77901d2f32139b59104d6f418f4fe6eab6176320
Your Name <jeffrey.sj.tang@gmail.com>
2024-06-18 15:57:24 -0700

    update global function definition

--- R/stats-cpca.R
+++ R/stats-cpca.R
@@ -13,7 +13,7 @@
 cpca <- function(eset, y, obs_id, db){
   pc <- t(sapply(db, function(ligand){
     genexp <- t(eset[intersect(rownames(eset), ligand), , drop=FALSE])
-    prcomp(genexp, center = TRUE, scale. = TRUE, rank. = 1)$x[, "PC1"]
+    stats::prcomp(genexp, center = TRUE, scale. = TRUE, rank. = 1)$x[, "PC1"]
   }))
   design <- create_design(y, obs_id)
   fit <- limma::eBayes(limma::lmFit(pc, design))
--- R/stats-cpcr.R
+++ R/stats-cpcr.R
@@ -13,7 +13,7 @@
 cpcr <- function(eset, y, obs_id, db){
   pcs <- sapply(db, function(ligand){
     genexp <- t(eset[intersect(rownames(eset), ligand), , drop=FALSE])
-    prcomp(genexp, center = TRUE, scale. = TRUE, rank. = 1)$x[, "PC1"]
+    stats::prcomp(genexp, center = TRUE, scale. = TRUE, rank. = 1)$x[, "PC1"]
   })
   fit <- mixOmics::plsda(pcs, y)
   coef <- abs(mixOmics::selectVar(fit, comp=1)$value$value.var)
