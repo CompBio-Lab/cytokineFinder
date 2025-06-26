3e911b955a99e101cceab242c52dadca7011c15b
Your Name <jeffrey.sj.tang@gmail.com>
2025-06-26 10:59:52 -0700

    create a databases.R file for database descriptions (also includes the beta coefficients from CytoSig). Remove the old ligand_receptor_db.rda

---
+++ R/databases.R
@@ -0,0 +1,17 @@
+#' Database List filtered on CytoSig-based ligands
+#'
+#' @format A list of lists containing CytoSig ligand sets for all database sources
+#' @source Various ligand-receptor interaction databases  
+"dbs_cytosig_ligands"
+
+#' Database List unfiltered
+#' 
+#' @format A list of lists containing LRIs for all database sources
+#' @source Various ligand receptor interaction databases
+"dbs_all"
+
+#' CytoSig Betas
+#' 
+#' @format A matrix of cytosig gene co-expression weights or betas
+#' @source From CytoSig
+"beta"
