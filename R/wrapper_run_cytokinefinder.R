73ec38739bf952a9edc8327a74796386d414f547
Your Name <jeffrey.sj.tang@gmail.com>
2025-06-26 09:14:56 -0700

    comment out old cytokinefinder script for testing purposes...

--- R/wrapper_run_cytokinefinder.R
+++ R/wrapper_run_cytokinefinder.R
@@ -19,61 +19,61 @@
 #' @importFrom future.apply future_lapply future_sapply
 #' @examples
 
-cytokinefinder <- function(eset, design, dbs, methods, 
-                           treatment = NULL, obs_id = NULL, correlation = NULL) { 
-  # Set up the future plan
-  future::plan(future::multicore)  # Set up multicore parallelism
-  
-  # Initialize an empty list to store results
-  results <- list()
-  
-  # Iterate over each method in the methods list
-  for (method_name in methods) {
-    method <- get(method_name)
-    
-    # Parallel execution for the current method
-    method_results <- future_lapply(names(dbs), function(database) {
-      message(paste("Processing method:", 
-                  method_name, 
-                  "with database:", 
-                  database)
-            )  # Debug statement
-      
-      # Check if the method is PLSDA-based:
-      if (grepl("plsda", method_name)) {
-        # Run the plsda method
-        result <- method(eset, treatment, dbs[[database]])
-      } else {
-        # Run non-PLSDA method
-        ## Check if it is a paired experiment by checking obs_id
-        if (!is.null(obs_id)) {
-          result <- method(eset, design, 
-                           dbs[[database]], 
-                           obs_id = obs_id,
-                           correlation = correlation)
-        } else{
-          result <- method(eset, design, dbs[[database]])
-          }
-      }
-      
-      message(paste("Finished processing method:", 
-                  method_name, 
-                  "with database:", 
-                  database)
-            )  # Debug statement
-      return(list(database = database, result = result))
-    }, future.seed = TRUE)  # Set future.seed to ensure reproducibility
-    
-    # Organize results into a named list
-    message(paste("Combining into one BenchmarkResults Object"))
-    method_results_named <- setNames(
-      lapply(method_results, `[[`, "result"), # extract method results
-      sapply(method_results, `[[`, "database") # extract database name
-      )
-    # Store the results in the main results list
-    results[[method_name]] <- method_results_named
-    }
-  
-  # Create and return results object
-  return(structure(results, class = "BenchmarkResults"))
-}
+# cytokinefinder <- function(eset, design, dbs, methods, 
+#                            treatment = NULL, obs_id = NULL, correlation = NULL) { 
+#   # Set up the future plan
+#   future::plan(future::multicore)  # Set up multicore parallelism
+#   
+#   # Initialize an empty list to store results
+#   results <- list()
+#   
+#   # Iterate over each method in the methods list
+#   for (method_name in methods) {
+#     method <- get(method_name)
+#     
+#     # Parallel execution for the current method
+#     method_results <- future_lapply(names(dbs), function(database) {
+#       message(paste("Processing method:", 
+#                   method_name, 
+#                   "with database:", 
+#                   database)
+#             )  # Debug statement
+#       
+#       # Check if the method is PLSDA-based:
+#       if (grepl("plsda", method_name)) {
+#         # Run the plsda method
+#         result <- method(eset, treatment, dbs[[database]])
+#       } else {
+#         # Run non-PLSDA method
+#         ## Check if it is a paired experiment by checking obs_id
+#         if (!is.null(obs_id)) {
+#           result <- method(eset, design, 
+#                            dbs[[database]], 
+#                            obs_id = obs_id,
+#                            correlation = correlation)
+#         } else{
+#           result <- method(eset, design, dbs[[database]])
+#           }
+#       }
+#       
+#       message(paste("Finished processing method:", 
+#                   method_name, 
+#                   "with database:", 
+#                   database)
+#             )  # Debug statement
+#       return(list(database = database, result = result))
+#     }, future.seed = TRUE)  # Set future.seed to ensure reproducibility
+#     
+#     # Organize results into a named list
+#     message(paste("Combining into one BenchmarkResults Object"))
+#     method_results_named <- setNames(
+#       lapply(method_results, `[[`, "result"), # extract method results
+#       sapply(method_results, `[[`, "database") # extract database name
+#       )
+#     # Store the results in the main results list
+#     results[[method_name]] <- method_results_named
+#     }
+#   
+#   # Create and return results object
+#   return(structure(results, class = "BenchmarkResults"))
+# }
