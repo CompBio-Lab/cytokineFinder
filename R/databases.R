21973d64c7b4ea2f4ec1b3e6e4085781362f4416
Your Name <jeffrey.sj.tang@gmail.com>
2025-07-11 11:07:36 -0700

    change the dbs in data and update database.R

--- R/databases.R
+++ R/databases.R
@@ -1,9 +1,3 @@
-#' Database List filtered on CytoSig-based ligands
-#'
-#' @format A list of lists containing CytoSig ligand sets for all database sources
-#' @source Various ligand-receptor interaction databases  
-"dbs_cytosig_ligands"
-
 #' Database List unfiltered
 #' 
 #' @format A list of lists containing LRIs for all database sources
