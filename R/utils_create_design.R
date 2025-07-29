c98cf4363b89f4d6f26c6e217c75235f291f6392
Your Name <jeffrey.sj.tang@gmail.com>
2025-07-28 20:14:12 -0700

    change indentation of create_design()

--- R/utils_create_design.R
+++ R/utils_create_design.R
@@ -16,13 +16,14 @@ create_design <- function(treatment, obs_id = NULL, eset = NULL){
   if(is.null(obs_id)) {
     #for unpaired datasets
     design <- model.matrix(~treatment)
-  } else{
-    design <- model.matrix(~treatment)
-    dupcor <- duplicateCorrelation(eset, 
+    
+    } else{
+      design <- model.matrix(~treatment)
+      dupcor <- duplicateCorrelation(eset, 
                                    design = model.matrix(~treatment), 
                                    block = obs_id)
-    message("Within-block correlation: ", dupcor$consensus.correlation)
-  }
+      message("Within-block correlation: ", dupcor$consensus.correlation)
+      }
   return(list(design = design, 
               dupcor = if (!is.null(eset) && !is.null(obs_id)) dupcor else NULL))
-}
+  }
